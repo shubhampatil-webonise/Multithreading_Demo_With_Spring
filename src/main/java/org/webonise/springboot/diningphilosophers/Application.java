276b59fd058b1849c14c9bec97a0545c2632b99c
shubhampatil-webonise <shubham.patil@weboniselab.com>
2016-09-30 19:31:58 +0530

    add implementation for dining philosophers problem using Spring Boot

---
+++ src/main/java/org/webonise/springboot/diningphilosophers/Application.java
@@ -0,0 +1,54 @@
+package org.webonise.springboot.diningphilosophers;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Component
+public class Application {
+    public static final int NUMBER_OF_PHILOSOPHERS = 5;
+    private static final Logger logger = Logger.getLogger(Application.class.getName());
+
+    private final Philosopher[] philosophers;
+    private final ReentrantLock[] chopsticks;
+
+    @Autowired
+    ThreadPoolExecutor threadPoolExecutor;
+
+    @Autowired
+    Application(@Qualifier("philosophers") Philosopher[] philosophers, @Qualifier("reentrantLocks") ReentrantLock[] reentrantLocks) {
+        this.philosophers = philosophers;
+        this.chopsticks = reentrantLocks;
+    }
+
+    public void start() {
+        startPhilosopherThreads();
+    }
+
+    private void startPhilosopherThreads() {
+        for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
+            philosophers[i].setId(i);
+            philosophers[i].setLeftChopstick(chopsticks[i]);
+            philosophers[i].setRightChopstick(chopsticks[(i + 1) % NUMBER_OF_PHILOSOPHERS]);
+
+            threadPoolExecutor.execute(philosophers[i]);
+        }
+
+        try {
+            Thread.sleep(10000);
+            threadPoolExecutor.shutdown();
+            for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
+                philosophers[i].finishEating();
+            }
+            threadPoolExecutor.awaitTermination(20, TimeUnit.SECONDS);
+
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
